2b4c2155de38f3d10ed1a1f23dcee085f72698f3
Elyza <elyzajanerelucio@gmail.com>
2023-04-18 20:12:48 +0800

    Input and output

---
+++ Decryption.py
@@ -0,0 +1,13 @@
+#Elyza Jane G. Relucio
+
+#pseudocode
+#ask the user to input encrypted text, then decrypt it
+input_encrypted_str = input("Enter a string to decrypt: ")
+output_decrypted_str = ""
+#check every character
+#if the character is *, change it to a
+#if the character is &, change it to e
+#if the character is #, change it to i
+#if the character is +, change it to o
+#if the character is !, change it to u
+#print the output
