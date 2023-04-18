d675b63dfdec17e1c8277c87b25947eb09f9d9bc
Elyza <elyzajanerelucio@gmail.com>
2023-04-18 21:06:20 +0800

    Decrypt using the given character substitute

--- Decryption.py
+++ Decryption.py
@@ -5,9 +5,23 @@
 input_encrypted_str = input("Enter a string to decrypt: ")
 output_decrypted_str = ""
 #check every character
+for i in range (len(input_encrypted_str)):
 #if the character is *, change it to a
+    if input_encrypted_str[i] == "*":
+        output_decrypted_str += "a"
 #if the character is &, change it to e
+    elif input_encrypted_str[i] == "&":
+        output_decrypted_str += "e"
 #if the character is #, change it to i
+    elif input_encrypted_str[i] == "#":
+        output_decrypted_str += "i"
 #if the character is +, change it to o
+    elif input_encrypted_str[i] == "#":
+        output_decrypted_str += "i"
 #if the character is !, change it to u
+    elif input_encrypted_str [i] == "!":
+        output_decrypted_str += "u"
+
+    else:
+        output_decrypted_str += input_encrypted_str[i]    
 #print the output
